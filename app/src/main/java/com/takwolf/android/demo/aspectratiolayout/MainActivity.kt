fe91af8097c307403194d2ea913b0e2ac18d5dc7
TakWolf <takwolf@foxmail.com>
2024-08-16 15:10:15 +0800

    调整样式

--- app/src/main/java/com/takwolf/android/demo/aspectratiolayout/MainActivity.kt
+++ app/src/main/java/com/takwolf/android/demo/aspectratiolayout/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.takwolf.android.demo.aspectratiolayout
 
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+        )
+        setContentView(R.layout.activity_main)
+    }
+}
