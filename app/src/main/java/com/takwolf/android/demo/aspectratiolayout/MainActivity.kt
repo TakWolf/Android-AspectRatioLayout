c1c7dfdb3e411b1cdeac2f037e1e8ca4c8b29857
TakWolf <takwolf@foxmail.com>
2025-02-03 21:38:20 +0800

    enableEdgeToEdge() 放到 onCreate() 之前

--- app/src/main/java/com/takwolf/android/demo/aspectratiolayout/MainActivity.kt
+++ app/src/main/java/com/takwolf/android/demo/aspectratiolayout/MainActivity.kt
@@ -8,11 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
         )
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
