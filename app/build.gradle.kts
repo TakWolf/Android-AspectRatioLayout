99f48c59c941220fc727e34f7f15a1fd73b531de
TakWolf <takwolf@foxmail.com>
2025-03-30 05:02:42 +0800

    升级 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.takwolf.android.demo.aspectratiolayout"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.takwolf.android.demo.aspectratiolayout"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.0.1"
     }
--- aspectratiolayout/build.gradle.kts
+++ aspectratiolayout/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.takwolf.android.aspectratiolayout"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 21
