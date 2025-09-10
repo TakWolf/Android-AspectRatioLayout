f19fbeae572a45278f68b99ffe31ede149cb7c06
TakWolf <takwolf@foxmail.com>
2025-09-11 06:07:05 +0800

    升级依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("com.github.TakWolf.Android-InsetsWidget:insetswidget:0.0.1")
     implementation(project(":aspectratiolayout"))
 }
