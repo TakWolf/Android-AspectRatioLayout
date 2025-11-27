04ad895260edc962b6afe3d08cd432979715c32b
TakWolf <takwolf@foxmail.com>
2025-11-28 02:56:15 +0800

    升级构建

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.takwolf.android.demo.aspectratiolayout"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
@@ -28,16 +29,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.4")
     implementation("io.github.takwolf.android.insetswidget:insetswidget:0.0.1")
     implementation(project(":aspectratiolayout"))
 }
